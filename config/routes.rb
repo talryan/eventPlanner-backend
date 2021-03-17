5c776ab9c33b3bdcf7d96d6a8fdef3ed961f5de9
talryan <nataliamryan@gmail.com>
2021-03-16 20:52:52 -0700

    Form POST works

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :clients do 
-  resources :events, only: [:index]
-end
+  resources :clients 
+  resources :events
 
-  resources :events: [:create, :show]
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  end
